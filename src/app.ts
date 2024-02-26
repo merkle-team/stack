a755e80850c35443fb9e708ebd516f883fa212a7
Shane da Silva <shane@dasilva.io>
2024-02-26 01:20:29 -0800

    Almost working deploy with zero-downtime switchover

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import {
   AutoScaling,
   AutoScalingGroup,
   Instance,
+  LifecycleState,
 } from "@aws-sdk/client-auto-scaling";
-import { EC2 } from "@aws-sdk/client-ec2";
+import { EC2, InstanceStateName } from "@aws-sdk/client-ec2";
 import { $ } from "bun";
 import { sleep } from "./util";
 
@@ -47,6 +48,8 @@ export class App {
           return undefined;
         });
       if (!ltExists?.LaunchTemplates?.length) {
+        // TODO: Include Docker Compose file preparation in launch template, and update launch template after each deploy
+
         console.debug("No launch template found, creating one...");
         const ltCreateResult = await ec2.createLaunchTemplate({
           LaunchTemplateName: launchTemplateName,
@@ -71,6 +74,8 @@ export class App {
             // TagSpecifications: [],
             UserData: Buffer.from(
               `#!/bin/bash
+set -x
+
 dnf update -y
 dnf install -y docker
 usermod -a -G docker ${HOST_USER}
@@ -106,16 +111,16 @@ EOF
           Version: "$Latest",
         },
 
-        MinSize: 1,
-        MaxSize: 1,
-        DesiredCapacity: 1,
+        MinSize: 2,
+        MaxSize: 2,
+        DesiredCapacity: 2,
 
         DefaultInstanceWarmup: 0, // AWS recommends to always set this, even if zero
         HealthCheckGracePeriod: 60,
 
         // InstanceMaintenancePolicy:
         // MaxInstanceLifetime:
-        NewInstancesProtectedFromScaleIn: true,
+        // NewInstancesProtectedFromScaleIn: true,
         // TerminationPolicies:
 
         Tags: [
@@ -223,21 +228,26 @@ EOF
       throw new Error(`No private IPs found for ASG ${asgName}`);
     }
 
+    console.info("Preparing release to the following IPs:", privateIps);
     await Promise.all(
-      privateIps.map(async (ip) => {
-        let attempts = 0;
-        while (attempts < MAX_ATTEMPTS) {
-          attempts += 1;
+      instanceDetails.map(async ({ PrivateIpAddress: ip }) => {
+        const startTime = Date.now();
+        while (Date.now() - startTime < 120_000) {
           try {
             const connectResult =
-              await $`ssh -o StrictHostKeychecking=accept-new -a ${HOST_USER}@${privateIps[0]} bash -s < ${new Response(
+              await $`ssh -o StrictHostKeychecking=accept-new -a ${HOST_USER}@${ip} bash -s < ${new Response(
                 `# Execute these commands on the remote server in a Bash shell
 set -e -o pipefail
 
-echo Connected to $(hostname -I | awk '{print $1}')
+echo Connected to ${ip}
+while [ ! -f /home/${HOST_USER}/.docker/config.json ]; do 
+  echo "Waiting for ${ip} to finish initializing via cloud-init..."
+  sleep 1
+done
 
-mkdir -p /home/${HOST_USER}/releases/${releaseId}
-cd /home/${HOST_USER}/releases/${releaseId}
+new_release_dir="/home/${HOST_USER}/releases/${releaseId}"
+mkdir -p "$new_release_dir"
+cd "$new_release_dir" 
 
 cat <<EOF > docker-compose.yml
 version: "3.8"
@@ -245,23 +255,27 @@ version: "3.8"
 services:
   test:
     image: caddy:2
+    command: ["caddy", "file-server", "--debug", "--browse"]
     #image: ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}-${pod}:${releaseId}
+    network_mode: host
     ports:
-      - "80"
-    restart: always
+      - "80:80"
+    restart: unless-stopped
     healthcheck:
-      disable: true
-    #  test: ["CMD", "curl", "-f", "http://localhost:80/"]
-    #  interval: 30s
-    #  timeout: 10s
-    #  retries: 3
-    #  start_period: 30s
+      test: ["CMD-SHELL", "printf 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n' | nc localhost 80"]
+      start_period: 5s
+      interval: 5s
+      timeout: 3s
+      retries: 3
     #logging:
     #  driver: "json-file"
     #  options:
     #    max-size: "10m"
     #    max-file: "5"
 EOF
+
+# Download/build any images we need to in preparation for the switchover (blocks until finished)
+docker compose up --no-start
             `,
               )}`;
             if (connectResult.exitCode !== 0) {
@@ -271,19 +285,128 @@ EOF
             }
             break; // Otherwise we were successful
           } catch (e: unknown) {
-            if (attempts >= MAX_ATTEMPTS) {
+            if (Date.now() - startTime > 120_000) {
               console.error(
-                `Unable to connect to ${ip} after ${MAX_ATTEMPTS} attempts. Aborting deploy`,
+                `Unable to connect to ${ip} after 2 minutes. Aborting deploy.`,
               );
               throw e;
             }
             console.warn(
-              `Unable to connect to ${ip} (attempt ${attempts}/${MAX_ATTEMPTS}). Retrying in 5 seconds...`,
+              `Unable to connect to ${ip}. Retrying in 5 seconds...`,
             );
             await sleep(5000);
           }
         }
       }),
     );
+
+    // Now that all instances have been prepared, perform the switchover
+    console.info("Deploying release to the following IPs:", privateIps);
+    for (const {
+      PrivateIpAddress: ip,
+      InstanceId: instanceId,
+    } of instanceDetails) {
+      const startTime = Date.now();
+      while (Date.now() - startTime < 120_000) {
+        try {
+          // Stop sending load balancer traffic to instance
+          await asg.enterStandby({
+            AutoScalingGroupName: asgName,
+            ShouldDecrementDesiredCapacity: true,
+            InstanceIds: [instanceId],
+          });
+
+          let beginTime = Date.now();
+          for (;;) {
+            let standbyInstances = await asg.describeAutoScalingInstances({
+              InstanceIds: [instanceId],
+            });
+            const standbyDetails = standbyInstances.AutoScalingInstances || [];
+            if (
+              standbyDetails.every(
+                (i) => i.LifecycleState === LifecycleState.STANDBY,
+              )
+            )
+              break;
+            if (Date.now() - beginTime > 60_000) {
+              throw new Error(
+                `Instance ${instanceId} (${ip}) did not enter Standby state within 60 seconds. Aborting deploy`,
+              );
+            }
+            console.info(
+              `Waiting for instance ${instanceId} (${ip}) to enter Standby state...`,
+            );
+            await sleep(5000);
+          }
+
+          const connectResult =
+            await $`ssh -a ${HOST_USER}@${ip} bash -s < ${new Response(
+              `# Execute these commands on the remote server in a Bash shell
+set -e -o pipefail
+
+# Stop the current release if there is one
+if [ -f /home/${HOST_USER}/releases/current && -d "$(cat /home/${HOST_USER}/releases/current)" ]; then
+  cd "$(cat /home/${HOST_USER}/releases/current)"
+  docker compose down --timeout 30 # Blocks until finished or timed out
+fi
+
+new_release_dir="/home/${HOST_USER}/releases/${releaseId}"
+cd "$new_release_dir" 
+
+# Start the pod and wait for all containers to be healthy
+docker compose up --detach --wait --wait-timeout 60
+
+# Update "current" location to point to the new release
+echo "$new_release_dir" > ../current
+          `,
+            )}`;
+          if (connectResult.exitCode !== 0) {
+            throw new Error(
+              `Error connecting to ${ip} (exit code ${connectResult.exitCode})`,
+            );
+          }
+
+          // Start sending load balancer traffic to instance again
+          await asg.exitStandby({
+            AutoScalingGroupName: asgName,
+            InstanceIds: [instanceId],
+          });
+
+          beginTime = Date.now();
+          for (;;) {
+            let standbyInstances = await asg.describeAutoScalingInstances({
+              InstanceIds: [instanceId],
+            });
+            const standbyDetails = standbyInstances.AutoScalingInstances || [];
+            if (
+              standbyDetails.every(
+                (i) => i.LifecycleState === LifecycleState.IN_SERVICE,
+              )
+            )
+              break;
+            if (Date.now() - beginTime > 60_000) {
+              throw new Error(
+                `Instance ${instanceId} (${ip}) did not enter InService state within 60 seconds. Aborting deploy`,
+              );
+            }
+            console.info(
+              `Waiting for instance ${instanceId} (${ip}) to enter InService state again...`,
+            );
+            await sleep(5000);
+          }
+
+          break; // Otherwise we were successful
+        } catch (e: unknown) {
+          if (Date.now() - startTime > 120_000) {
+            console.error(
+              `Unable to deploy to ${ip} after 2 minutes. Aborting deploy.`,
+            );
+            throw e;
+          }
+          console.warn(`Unable to deploy to ${ip}. Retrying in 5 seconds...`);
+          await sleep(5000);
+        }
+      }
+    }
   }
 }
